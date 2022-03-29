b839b3e4ac78fbd565efe404b556ea3c72820304
PavanGangireddy <pavangangireddy@ibhubs.co>
2022-03-29 09:58:56 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,82 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+import { useState } from 'react'
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseAdmin = createClient(
+  'https://szomjeqqtgwdeuxynbjl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6b21qZXFxdGd3ZGV1eHluYmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg1MjUyNTAsImV4cCI6MTk2NDEwMTI1MH0.BVZLzRumwIqsGYi18_Y2wZ9ZdJI0pKLp0u2v8NsaQX4'
+)
+
+// const seedData = async () => {
+//   await supabaseAdmin.from('images').insert([
+//     {
+//       name: 'Pedro Duarte',
+//       href: 'https://twitter.com/peduarte/status/1463897468383412231',
+//       userName: '@peduarte',
+//       imgSrc: 'https://pbs.twimg.com/media/FFDOtLkWYAsWjTM?format=jpg',
+//     },
+//   ])
+// }
+
+// seedData()
+
+export async function getStaticProps() {
+  const { data } = await supabaseAdmin.from('images').select('*')
+  return {
+    props: {
+      images: data,
+    },
+  }
+}
+
+type Image = {
+  id: number
+  href: string
+  imgSrc: string
+  name: string
+  userName: string
+}
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image} />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Home
+function BlurImage({ image }: { image: Image }) {
+  const [isLoading, setLoading] = useState(true)
+  const { imgSrc, href, name, userName } = image
+  return (
+    <a href={href} className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+        <Image
+          alt=""
+          src={imgSrc}
+          layout="fill"
+          objectFit="cover"
+          className={cn(
+            'duration-700 ease-in-out group-hover:opacity-75',
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          )}
+          onLoadingComplete={() => setLoading(false)}
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{userName}</p>
+    </a>
+  )
+}
