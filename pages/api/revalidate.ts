087e37c26d0d254443c9c0968a9e0d6981a114aa
PavanGangireddy <pavangangireddy@ibhubs.co>
2022-03-29 16:31:57 +0530

    added types

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,4 +1,9 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
